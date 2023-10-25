ae189c4ee5e4727ccb334e3dc465b4d37f835f4d
Gabriel Feitosa <gabriel.feitosa@merx.tech>
2023-10-25 18:39:44 -0300

    improvement

--- src/app/search/components/SearchSideBar.tsx
+++ src/app/search/components/SearchSideBar.tsx
@@ -1,38 +1,90 @@
-import { Cuisine, Location } from "@prisma/client";
+import { Cuisine, Location, PRICE } from "@prisma/client";
+import Link from "next/link";
 
 export default function SearchSideBar({
   location,
   cuisine,
+  searchParams,
 }: {
   location: Location[];
   cuisine: Cuisine[];
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
 }) {
   return (
     <div className="w-1/5">
-      <div className="border-b pb-4">
+      <div className="border-b pb-4 flex flex-col">
         <h1 className="mb-2">Region</h1>
         {location.map((location) => (
-          <p className="font-light text-reg capitalize">{location.name}</p>
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                city: location.name,
+              },
+            }}
+            className="font-light text-reg capitalize"
+          >
+            {location.name}
+          </Link>
         ))}
       </div>
-      <div className="border-b pb-4 mt-3">
+      <div className="border-b pb-4 mt-3 flex flex-col">
         <h1 className="mb-2">Cuisine</h1>
         {cuisine.map((cuisine) => (
-          <p className="font-light text-reg capitalize">{cuisine.name}</p>
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                cuisine: cuisine.name,
+              },
+            }}
+            className="font-light text-reg capitalize"
+          >
+            {cuisine.name}
+          </Link>
         ))}
       </div>
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
         <div className="flex">
-          <button className="border w-full text-reg font-light rounded-l p-2">
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                price: PRICE.CHEAP,
+              },
+            }}
+            className="border w-full text-reg font-light rounded-l p-2"
+          >
             $
-          </button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2">
+          </Link>
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                price: PRICE.REGULAR,
+              },
+            }}
+            className="border-r border-t border-b w-full text-reg font-light p-2"
+          >
             $$
-          </button>
-          <button className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
+          </Link>
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                price: PRICE.EXPENSIVE,
+              },
+            }}
+            className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r"
+          >
             $$$
-          </button>
+          </Link>
         </div>
       </div>
     </div>
